78882a0023d5f0104f5a8ca78e2bb7aae0267514
khaledismaeel <k.ismaeel@innopolis.university>
2021-08-18 14:09:42 +0300

    Add JavaDoc comments to Enforceable interface, remove redundant static modifier and semicolon

--- src/stig/Enforceable.java
+++ src/stig/Enforceable.java
@@ -1,9 +1,17 @@
 package stig;
 
+/**
+ * Implementations of this interface are requirements that can enforced on the hosting environment programmatically
+ * through the enforce function.
+ */
 public interface Enforceable {
-    static enum EnforcementStatus {
+    enum EnforcementStatus {
         SUCCESS, FAILURE, INCOMPLETE
-    };
+    }
 
+    /**
+     * Modifies the hosting environment to satisfy the requirement.
+     * @return The outcome of the enforcement process or whether it was unable to complete.
+     */
     EnforcementStatus enforce();
 }
