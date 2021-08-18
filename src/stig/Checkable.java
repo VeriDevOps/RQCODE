9cca2646a2864e191b20f2e2904938ed23ce3735
khaledismaeel <k.ismaeel@innopolis.university>
2021-08-18 14:05:13 +0300

    Add JavaDoc comments to Checkable interface, remove redundant static modifier and semicolon

--- src/stig/Checkable.java
+++ src/stig/Checkable.java
@@ -1,9 +1,16 @@
 package stig;
 
+/**
+ * Implementations of this interface are requirements that can be checked programmatically through the check function.
+ */
 public interface Checkable {
-    static enum CheckStatus {
+    enum CheckStatus {
         PASS, FAIL, INCOMPLETE
-    };
+    }
 
+    /**
+     * Checks whether the current environment satisfies the requirement of not.
+     * @return The result of the check or whether the check was unable to be complete.
+     */
     CheckStatus check();
 }
