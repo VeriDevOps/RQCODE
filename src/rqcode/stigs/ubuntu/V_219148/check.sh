938e52c1628b20fd087426189da262f712883251
Alexandr Naumchev <anaumchev@gmail.com>
2022-03-10 13:52:38 +0000

    Inverting the checks

--- src/rqcode/stigs/ubuntu/V_219148/check.sh
+++ src/rqcode/stigs/ubuntu/V_219148/check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://www.stigviewer.com/stig/canonical_ubuntu_18.04_lts/2021-06-16/finding/V-219148
 value_to_test=`grep –i password /boot/efi/EFI/grub.cfg | cut -d' ' -f1`
-if [ $value_to_test == 'password_pbkdf2' ]
+if [ $value_to_test != 'password_pbkdf2' ]
 then
-    exit 0
+    exit -1
 fi
-exit -1
+exit 0
