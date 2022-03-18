0deaf93923586bac269fd99191decf950c5aed42
Ildar1 <ildarnn22@gmail.com>
2022-03-18 14:40:13 +0300

    Added V_63863.java

--- src/rqcode/concepts/Windows10SecurityTechnicalImplementationGuide.java
+++ src/rqcode/concepts/Windows10SecurityTechnicalImplementationGuide.java
@@ -9,6 +9,8 @@ public class Windows10SecurityTechnicalImplementationGuide {
     public static final CheckableEnforceableRequirement v_63467 = new V_63467();
     public static final CheckableEnforceableRequirement v_63851 = new v_63851();
     public static final CheckableEnforceableRequirement v_63931 = new v_63931();
+    public static final CheckableEnforceableRequirement v_63843 = new v_63843();
+    public static final CheckableEnforceableRequirement v_63863 = new v_63863();
     public static CheckableEnforceableRequirement[] allSTIGs() {
         CheckableEnforceableRequirement[] retArr = {
             v_63447,
@@ -16,8 +18,9 @@ public class Windows10SecurityTechnicalImplementationGuide {
             v_63463,
             v_63467,
             v_63851,
-            v_63931
-
+            v_63931,
+            v_63843,
+            v_63863
         };
         return retArr;
     }
---
+++ src/rqcode/stigs/win10/V_63863.java
@@ -0,0 +1,64 @@
+package patterns;
+
+public class V_63863 extends UserAccountManagementRequirement {
+    @Override
+    protected String getFailure() {
+        return null;
+    }
+
+    @Override
+    protected String getInclusionSetting() {
+        return "Success";
+    }
+
+    @Override
+    protected String getSuccess() {
+        return "enable";
+    }
+
+    @Override
+    public String checkTextCode() {
+        return "C-64613r1_chk";
+    }
+
+    @Override
+    public String date() {
+        return "2019-01-04";
+    }
+
+    @Override
+    public String findingID() {
+        return "V-63863";
+    }
+
+    @Override
+    public String fixTextCode() {
+        return "F-69791r1_fix";
+    }
+
+    @Override
+    public String iAControls() {
+        return "";
+    }
+
+    @Override
+    public String ruleID() {
+        return "SV-78353r1_rule";
+    }
+
+    @Override
+    public String sTIG() {
+        return "Windows 10 Security Technical Implementation Guide";
+    }
+
+    @Override
+    public String severity() {
+        return "Medium";
+    }
+
+    @Override
+    public String version() {
+        return "WN10-UR-000055";
+    }
+}
+
