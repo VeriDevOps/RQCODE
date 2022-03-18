6f005ffb448dc946f1ad94ee651feda7a8e52a84
Ildar1 <ildarnn22@gmail.com>
2022-03-18 14:01:46 +0300

    Fixed name of classe and added V_63851 and V_63931 in Windows10SecurityTechnicalImplementationGuide.java

--- src/rqcode/concepts/Windows10SecurityTechnicalImplementationGuide.java
+++ src/rqcode/concepts/Windows10SecurityTechnicalImplementationGuide.java
@@ -4,15 +4,20 @@ import patterns.*;
 
 public class Windows10SecurityTechnicalImplementationGuide {
     public static final CheckableEnforceableRequirement v_63447 = new V_63447();
-    public static final CheckableEnforceableRequirement v_63779 = new V_63449();
+    public static final CheckableEnforceableRequirement v_63449 = new V_63449();
     public static final CheckableEnforceableRequirement v_63463 = new V_63463();
     public static final CheckableEnforceableRequirement v_63467 = new V_63467();
+    public static final CheckableEnforceableRequirement v_63851 = new v_63851();
+    public static final CheckableEnforceableRequirement v_63931 = new v_63931();
     public static CheckableEnforceableRequirement[] allSTIGs() {
         CheckableEnforceableRequirement[] retArr = {
             v_63447,
-            v_63779,
+            v_63449,
+            v_63463,
             v_63467,
-            v_63779
+            v_63851,
+            v_63931
+
         };
         return retArr;
     }
