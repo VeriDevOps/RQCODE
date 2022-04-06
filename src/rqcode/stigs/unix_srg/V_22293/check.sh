3033a7c4b3dd95f3e6008f83a52e595119a10f5a
Alexandr Naumchev <anaumchev@gmail.com>
2022-04-06 05:11:58 +0000

    Fixed the no crontasks case

--- src/rqcode/stigs/unix_srg/V_22293/check.sh
+++ src/rqcode/stigs/unix_srg/V_22293/check.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # https://www.stigviewer.com/stig/unix_srg/2013-03-26/finding/V-22293
+crons_count=`crontab -l 2>/dev/null | wc -l`
+if [ $crons_count -eq 0 ]
+then
+    echo "No crontask found for time synchronization"
+    exit -1
+fi
 ntpdate_invoked_correctly=`crontab -l | grep ntpdate | grep '-a' | wc -l`
 if [ $ntpdate_invoked_correctly -eq 0 ]
 then
+    echo "The ntpdate command is not invoked with the -a parameter"
     exit -1
 fi
-ntpdate_key_config=`grep ^server ntp.conf | grep -v '( key | autokey )' | wc -l`
+ntpdate_key_config=`grep "^server" ntp.conf | grep -v '( key | autokey )' | wc -l`
 if [ $ntpdate_key_config -eq 0 ]
 then
+    echo "Server lines are present without key or autokey options"
     exit -1
 fi
 exit 0
