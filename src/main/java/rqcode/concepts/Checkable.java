58b09ae4c6ed31f4cb1227e0857adf1198ecc2ba
Ildar1 <ildarnn22@gmail.com>
2022-09-02 09:30:11 +0300

    Requirement class comments updates

--- src/main/java/rqcode/concepts/Checkable.java
+++ src/main/java/rqcode/concepts/Checkable.java
@@ -6,13 +6,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
  * Implementations of this interface are requirements that can be checked programmatically through the check function.
  */
 @objid ("111730fe-edfd-4d16-bb51-8503832a3d5c")
-public interface Checkable {
-    /**
-     * Checks whether the current environment satisfies the requirement of not.
-     * @return The result of the check or whether the check was unable to be complete.
-     */
-    @objid ("6bda9775-ea96-484d-b457-749ee72b4efa")
-    CheckStatus check();
+c
 
     @objid ("8f66df5b-0cba-4b54-9972-df49c63bda82")
     public enum CheckStatus {
