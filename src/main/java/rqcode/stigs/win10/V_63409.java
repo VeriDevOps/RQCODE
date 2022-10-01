e033bc1707f6f7d3ffa48c03bb7e7b7cfc30bab3
Ildar1 <ildarnn22@gmail.com>
2022-10-02 00:14:08 +0200

    Added V-63409 class description

--- src/main/java/rqcode/stigs/win10/V_63409.java
+++ src/main/java/rqcode/stigs/win10/V_63409.java
@@ -2,6 +2,14 @@ package rqcode.stigs.win10;
 
 import rqcode.patterns.win10.AccountLockoutPolicy;
 
+/**
+ *The account lockout feature, when enabled, prevents brute-force password attacks on the system.
+ * The higher this value is, the less effective the account lockout feature will be in protecting the local system.
+ * The number of bad logon attempts must be reasonably small to minimize the possibility of a successful password attack, while allowing for honest errors made during a normal user logon.
+ *
+ * https://www.stigviewer.com/stig/windows_10/2016-10-28/finding/V-63409
+ */
+
 public class V_63409 extends AccountLockoutPolicy {
     @Override
     public String checkTextCode() {
