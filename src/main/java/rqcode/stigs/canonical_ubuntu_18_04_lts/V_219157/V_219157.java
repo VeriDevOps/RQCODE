c6209c8af1647c4ee2ab17226cda3baa20ac1445
Andrey Sadovykh <sadovykh@gmail.com>
2023-01-18 17:55:43 +0100

    updates

--- src/main/java/rqcode/stigs/canonical_ubuntu_18_04_lts/V_219157/V_219157.java
+++ src/main/java/rqcode/stigs/canonical_ubuntu_18_04_lts/V_219157/V_219157.java
@@ -1,28 +1,20 @@
 package rqcode.stigs.canonical_ubuntu_18_04_lts.V_219157;
 
-
-
-import rqcode.concepts.Checkable;
 import rqcode.stigs.canonical_ubuntu_18_04_lts.UbuntuPackagePattern;
 
 /**
- * Removing the Network Information Service (NIS) package decreases the risk of the accidental (or intentional) activation of NIS or NIS+ services.
+ * The Ubuntu operating system must not have the Network Information Service
+ * (NIS) package installed.
+ * 
+ * Removing the Network Information Service (NIS) package decreases the risk of
+ * the accidental (or intentional) activation of NIS or NIS+ services.
  * 
  * https://www.stigviewer.com/stig/canonical_ubuntu_18.04_lts/2021-06-16/finding/V-219157
  */
 
-public class V_219157 implements Checkable {
-    
-    private UbuntuPackagePattern _package = new UbuntuPackagePattern("nis", false);
+public class V_219157 extends UbuntuPackagePattern {
 
-    
-    public CheckStatus check() {
-        return _package.check();
+    public V_219157() {
+        super("nis", false);
     }
-
-    
-    public String toString() {
-        return _package.toString();
-    }
-
 }
