68c5ebbaa7d0b78326f3bef5f93de6984fdaaf04
Andrey Sadovykh <sadovykh@gmail.com>
2023-02-28 16:59:40 +0100

    minor re-formating

--- src/main/java/rqcode/example/TickBoundaryRequirement.java
+++ src/main/java/rqcode/example/TickBoundaryRequirement.java
@@ -10,11 +10,9 @@ import rqcode.concepts.Requirement;
  * https://www.sciencedirect.com/science/article/abs/pii/S1477842416301981
  */
 public class TickBoundaryRequirement extends Requirement {
-
     public TickBoundaryRequirement() {
         super("Clock seconds must be between 0 and 59.");
     }
-
     @Override
     public CheckStatus check() {
         if (Clock.seconds > 59) return CheckStatus.FAIL;
--- src/main/java/rqcode/example/TickIncrementRequirement.java
+++ src/main/java/rqcode/example/TickIncrementRequirement.java
@@ -10,17 +10,14 @@ import rqcode.concepts.Requirement;
  * https://www.sciencedirect.com/science/article/abs/pii/S1477842416301981
  */
 public class TickIncrementRequirement extends Requirement {
-
     public TickIncrementRequirement() {
         super("A clock tick increments current second if it is smaller than 59");
     }
-
     @Override
     public CheckStatus check() {
         if (Clock.seconds < 59)
             return ((Clock.seconds + 1) == Clock.tick()) ? 
-            CheckStatus.PASS : CheckStatus.FAIL;
-        
+            CheckStatus.PASS : CheckStatus.FAIL;        
         return CheckStatus.INCOMPLETE;
     }
 }
