c1147b4c063eb49d1427c57f5d090cf5271aa7aa
Andrey Khoroshavin <47006007+Andrew174194@users.noreply.github.com>
2023-06-10 21:37:41 +0300

    Delete separate check.sh files for UbuntuPackagePattern STIGs
    Since classes that extend UbuntuPackagePattern use package.sh as a test script, there is no need in separate check.sh scripts for every of them

--- src/main/java/rqcode/stigs/canonical_ubuntu_18_04_lts/V_219158/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://www.stigviewer.com/stig/canonical_ubuntu_18.04_lts/2021-06-16/finding/V-219158
-is_rsh_server_installed=`dpkg -l | grep rsh-server | awk '{print $2}' | grep -x rsh-server | wc -l`
-if [ $is_rsh_server_installed -ne 0 ]
-then
-    exit -1
-fi
-exit 0
-# The Ubuntu operating system must not have the rsh-server package installed.
