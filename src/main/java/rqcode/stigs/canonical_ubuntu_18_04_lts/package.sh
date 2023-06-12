c8786e43c79b47ba133e9872953497d6ee5ce19f
Andrey Khoroshavin <47006007+Andrew174194@users.noreply.github.com>
2023-06-13 02:16:26 +0300

    Support of not exact package name for installation test
    Changed regex allows the packages named like "package:arch" (e.g. opensc-pkcs11:arm64) to pass the test
    
    By this way, test should pass even if package has upstream_version after the epoch

--- src/main/java/rqcode/stigs/canonical_ubuntu_18_04_lts/package.sh
+++ src/main/java/rqcode/stigs/canonical_ubuntu_18_04_lts/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 package_name=$1
-is_installed=`dpkg -l | grep $package_name | awk '{print $2}' | grep -x $package_name | wc -l`
+is_installed=`dpkg -l | grep $package_name | awk '{print $2}' | grep -x "$package_name\|$package_name:.*" | wc -l`
 if [ $is_installed -eq 0 ]
 then
     exit -1
