3b4ed1530c3485dd6785a6721a8053969bf49543
ildarnn22@gmail.com <ildarnn22@gmail.com>
2023-06-13 00:01:02 +0200

    Added V_63703 STIG class

---
+++ src/main/java/rqcode/stigs/win10_new/RegistryEdit/stigs/V_63703.java
@@ -0,0 +1,26 @@
+package rqcode.stigs.win10_new.RegistryEdit.stigs;
+import rqcode.stigs.win10_new.RegistryEdit.RegistryEditConst;
+import rqcode.stigs.win10_new.RegistryEdit.RegistryEditPattern;
+import rqcode.stigs.win10_new.RegistryEdit.RegistryEditScriptPattern;
+import rqcode.stigs.win10_new.patterns.STIGScriptPattern;
+
+
+public class V_63703 extends RegistryEditPattern {
+    private final RegistryEditScriptPattern pattern =
+            new RegistryEditScriptPattern(RegistryEditConst.REGISTRY_EDIT_SCRIPT_PATTERN,
+                    "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanWorkstation\\Parameters",
+                    "RequireSecuritySignature",
+                    "DWORD",
+                    "1");
+
+
+    @Override
+    public STIGScriptPattern pattern() {
+        return pattern;
+    }
+
+
+
+
+
+}
---
+++ src/test/java/rqcode/stigs/win10_new/AuditPolicy/EnforcementStatusTest.java
@@ -0,0 +1,22 @@
+package rqcode.stigs.win10_new.AuditPolicy;
+import org.junit.Test;
+import rqcode.concepts.Enforceable;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnforcementStatusTest {
+
+    @Test
+    public void testEnforcementStatusSuccess() {
+      Enforceable.EnforcementStatus status = Enforceable.EnforcementStatus.SUCCESS;
+        assertEquals("Expected EnforcementStatus to be SUCCESS", status, Enforceable.EnforcementStatus.SUCCESS);
+    }
+
+    @Test
+    public void testEnforcementStatusFailure() {
+        Enforceable.EnforcementStatus status = Enforceable.EnforcementStatus.FAILURE;
+        assertEquals("Expected EnforcementStatus to be FAILURE", status, Enforceable.EnforcementStatus.FAILURE);
+    }
+
+}
+
