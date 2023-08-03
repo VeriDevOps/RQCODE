6bc6fc412a0726a0c209879ab901d3c0090cfad2
ildarnn22@gmail.com <ildarnn22@gmail.com>
2023-08-03 09:07:40 +0200

    Resolve conflict in RegistryEditPattern

--- src/main/java/rqcode/stigs/win10_new/RegistryEdit/RegistryEditPattern.java
+++ src/main/java/rqcode/stigs/win10_new/RegistryEdit/RegistryEditPattern.java
@@ -3,27 +3,26 @@ package rqcode.stigs.win10_new.RegistryEdit;
 import rqcode.stigs.win10_new.patterns.STIGPattern;
 import rqcode.stigs.win10_new.patterns.STIGScriptPattern;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public abstract class RegistryEditPattern implements STIGPattern {
+public class RegistryEditPattern implements STIGPattern {
 
     @Override
     public CheckStatus check() {
+        String settingRegistryPath = pattern().getRegistryPath();
         String settingName = pattern().getSettingName();
         String settingValue = pattern().getSettingValue();
+        String settingValueType = pattern().getSettingValueType();
 
         String script = pattern().prepareCheckScript();
 
         boolean registryEditCheck;
         try {
-            registryEditCheck = checkProcess(script, settingName, settingValue);
+            registryEditCheck = checkProcess(script, RegistryPath, settingName, settingValue);
         } catch (Exception e) {
             e.printStackTrace();
             return CheckStatus.INCOMPLETE;
         }
 
-        if (registryEditCheck)
+        if (auditPolicyCheck)
             return CheckStatus.PASS;
         else
             return CheckStatus.FAIL;
@@ -31,33 +30,16 @@ public abstract class RegistryEditPattern implements STIGPattern {
 
     @Override
     public EnforcementStatus enforce() {
-        String script = pattern().prepareEnforceScript();
+        return null;
+    }
 
-        try {
-            Process process = Runtime.getRuntime().exec(script);
-            process.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return EnforcementStatus.FAILURE;
-        }
-        return EnforcementStatus.SUCCESS;
+    @Override
+    public STIGScriptPattern pattern() {
+        return null;
     }
 
     @Override
     public boolean checkProcess(String script, String settingName, String settingValue) throws Exception {
-        Process regEdit = Runtime.getRuntime().exec(script);
-        BufferedReader regEditOutputReader = new BufferedReader(new InputStreamReader(regEdit.getInputStream()));
-
-        StringBuilder processOutput = new StringBuilder();
-        String inputLine;
-
-        while ((inputLine = regEditOutputReader.readLine()) != null) {
-            processOutput.append(inputLine + System.lineSeparator());
-        }
-
-        String result = processOutput.toString().toLowerCase();
-        boolean flag = settingValue.equals("enable");
-
-        return result.contains(settingName) && flag;
+        return true;
     }
 }
