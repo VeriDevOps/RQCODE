d0558e7c1075b85340f44c6298ce90e5a585bb10
Andrey Sadovykh <sadovykh@gmail.com>
2023-08-03 16:17:23 +0200

    Main added

---
+++ src/main/java/rqcode/stigs/win10/MainIterative.java
@@ -0,0 +1,18 @@
+package rqcode.stigs.win10;
+
+import rqcode.stigs.STIG;
+
+public class MainIterative {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        STIG[] allSTIGs = Windows10SecurityTechnicalImplementationGuide.allSTIGs();
+
+        for (STIG stig : allSTIGs){
+            System.out.println(stig.toString());
+             System.out.println(stig.check());
+        }
+        
+    }
+}
