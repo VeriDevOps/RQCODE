8a985ad40949515586dcf7cee540c088f4980f68
ildarnn22@gmail.com <ildarnn22@gmail.com>
2023-10-10 16:23:30 +0200

    refactored V_63469

--- src/main/java/rqcode/stigs/win10_new/Windows10SecurityTechnicalImplementationGuide.java
+++ src/main/java/rqcode/stigs/win10_new/Windows10SecurityTechnicalImplementationGuide.java
@@ -1,9 +1,6 @@
 package rqcode.stigs.win10_new;
 
-import rqcode.stigs.win10_new.AuditPolicy.stigs.V_63447;
-import rqcode.stigs.win10_new.AuditPolicy.stigs.V_63449;
-import rqcode.stigs.win10_new.AuditPolicy.stigs.V_63463;
-import rqcode.stigs.win10_new.AuditPolicy.stigs.V_63467;
+import rqcode.stigs.win10_new.AuditPolicy.stigs.*;
 import rqcode.stigs.win10_new.RegistryEdit.stigs.V_63321;
 import rqcode.stigs.win10_new.RegistryEdit.stigs.V_63647;
 import rqcode.stigs.win10_new.RegistryEdit.stigs.V_63703;
@@ -23,6 +20,8 @@ public class Windows10SecurityTechnicalImplementationGuide {
 
     public static final STIGPattern v_63647 = new V_63647();
 
+    public static final STIGPattern v_63469 = new V_63469();
+
     public static void main(String[] args) {
         System.out.println(v_63447.check());
         System.out.println(v_63449.check());
@@ -32,5 +31,6 @@ public class Windows10SecurityTechnicalImplementationGuide {
         System.out.println(v_63703.check());
         System.out.println(v_63321.check());
         System.out.println(v_63647.check());
+        System.out.println(v_63469.check());
     }
 }
