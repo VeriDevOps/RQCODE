72d2bae96f9ee81d0b8739ef1abdd5737e48f459
ildarnn22@gmail.com <ildarnn22@gmail.com>
2023-10-10 16:28:47 +0200

    refactored V_63469

--- src/main/java/rqcode/stigs/win10_new/AuditPolicy/AuditPolicyConst.java
+++ src/main/java/rqcode/stigs/win10_new/AuditPolicy/AuditPolicyConst.java
@@ -2,7 +2,7 @@ package rqcode.stigs.win10_new.AuditPolicy;
 
 public class AuditPolicyConst {
     public static final String AUDIT_POLICY_SCRIPT_PATTERN_ENFORCE = "auditpol /set /subcategory:\"%(guid)\" /%(parameter):%(value)";
-    public static final String AUDIT_POLICY_SCRIPT_PATTERN_CHECK = "auditpol /get /subcategory:\"%(guid)\"
+    public static final String AUDIT_POLICY_SCRIPT_PATTERN_CHECK = "auditpol /get /subcategory:\"%(guid)\"";
 
 }
 
