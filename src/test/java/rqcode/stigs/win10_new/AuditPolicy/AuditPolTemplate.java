fbaba885860c643371d376a641a9b731b58f65fc
ildarnn22@gmail.com <ildarnn22@gmail.com>
2023-11-04 23:15:06 +0100

    Added test for AuditPol class generating

---
+++ src/test/java/rqcode/stigs/win10_new/AuditPolicy/AuditPolTemplate.java
@@ -0,0 +1,40 @@
+package rqcode.stigs.win10_new.AuditPolicy;
+
+public class AuditPolTemplate {
+    private String id;
+    private String parameter;
+    private String value;
+    private String guid;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+}
