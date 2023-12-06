2e66c087a0cd74d4a59c4f277ed63810756d9429
Andrey Sadovykh <sadovykh@gmail.com>
2023-12-06 11:22:32 +0100

    fixes

--- src/main/java/rqcode/stigs/win10/patterns/AccountManagementRequirement.java
+++ src/main/java/rqcode/stigs/win10/patterns/AccountManagementRequirement.java
@@ -1,17 +1,11 @@
 package rqcode.stigs.win10.patterns;
 
-import java.util.Map;
-
 /**
- * General STIG requirement pattern for checking settings of Win10 User Account Management functionality.
+ * General STIG requirement pattern for checking settings of Win10 User Account
+ * Management functionality.
  */
 
 public abstract class AccountManagementRequirement extends AuditPolicyRequirement {
-    
-    public AccountManagementRequirement(Map<String, String> stigInfo) {
-        super(stigInfo);
-        //TODO Auto-generated constructor stub
-    }
 
     @Override
     protected String getCategory() {
