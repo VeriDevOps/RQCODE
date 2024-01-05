b111d3d961c2316a60149820b9e664469b327cd4
ildar1 <ildarnn22@gmail.com>
2024-01-05 03:39:46 +0300

    Fixed Check for User Rights category

--- src/main/java/rqcode/stigs/win10_new/Windows10SecurityTechnicalImplementationGuide.java
+++ src/main/java/rqcode/stigs/win10_new/Windows10SecurityTechnicalImplementationGuide.java
@@ -6,6 +6,8 @@ import rqcode.stigs.win10_v3.UserRights.V_220969;
 import rqcode.stigs.win10_v3.UserRights.V_220982;
 import rqcode.stigs.win10_v3.UserRights.V_220975;
 import rqcode.stigs.win10_v3.UserRights.V_220968;
+import rqcode.stigs.win10_v3.RegEdit.V_220870;
+import rqcode.stigs.win10_v3.RegEdit.V_220871;
 
 
 public class Windows10SecurityTechnicalImplementationGuide {
@@ -13,7 +15,8 @@ public class Windows10SecurityTechnicalImplementationGuide {
     public static final STIG v_220982 = new V_220982();
     public static final STIG v_220975 = new V_220975();
     public static final STIG v_220968 = new V_220968();
- 
+    public static final STIG v_220870 = new V_220870();
+    public static final STIG v_220871 = new V_220871();
 
     
     public static void main(String[] args) {
@@ -25,7 +28,10 @@ public class Windows10SecurityTechnicalImplementationGuide {
         System.out.println(STIGPatternMessage.fullMessageCheck(v_220975, v_220975.check()));
         System.out.println(STIGPatternMessage.fullMessageEnforce(v_220968, v_220968.enforce()));
         System.out.println(STIGPatternMessage.fullMessageCheck(v_220968, v_220968.check()));
-
+        System.out.println(STIGPatternMessage.fullMessageEnforce(v_220870, v_220870.enforce()));
+        System.out.println(STIGPatternMessage.fullMessageCheck(v_220870, v_220870.check()));
+        System.out.println(STIGPatternMessage.fullMessageEnforce(v_220871, v_220871.enforce()));
+        System.out.println(STIGPatternMessage.fullMessageCheck(v_220871, v_220871.check()));
 
 
     }
