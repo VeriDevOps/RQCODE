7f7ad7e92c59e5fcea3df31f0c2b70c23371d4f1
ildar1 <ildarnn22@gmail.com>
2024-01-09 02:39:07 +0300

    refactored main() for Win_v3

---
+++ src/main/java/rqcode/stigs/win10_v3/Windows10SecurityTechnicalImplementationGuide.java
@@ -0,0 +1,39 @@
+package rqcode.stigs.win10_v3;
+
+import rqcode.stigs.STIG;
+import rqcode.stigs.win10_new.patterns.STIGPatternMessage;
+import rqcode.stigs.win10_v3.UserRights.V_220969;
+import rqcode.stigs.win10_v3.UserRights.V_220974;
+import rqcode.stigs.win10_v3.UserRights.V_220975;
+import rqcode.stigs.win10_v3.UserRights.V_220972;
+import rqcode.stigs.win10_v3.UserRights.V_220957;
+import rqcode.stigs.win10_v3.UserRights.V_220982;
+
+
+public class Windows10SecurityTechnicalImplementationGuide {
+    public static final STIG v_220969 = new V_220969();
+    public static final STIG v_220974 = new V_220974();
+    public static final STIG v_220975 = new V_220975();
+    public static final STIG v_220972 = new V_220972();
+    public static final STIG v_220957 = new V_220957();
+    public static final STIG v_220982 = new V_220982();
+    
+    public static void main(String[] args) {
+        System.out.println(v_220982.enforce());
+        System.out.println(v_220982.check());
+        System.out.println(v_220982.toString());
+        //System.out.println(v_220969.check());
+        //System.out.println(STIGPatternMessage.fullMessageCheck(v_220972, v_220972.check()));
+       // System.out.println(STIGPatternMessage.fullMessageEnforce(v_220975, v_220975.enforce()));
+       // System.out.println(STIGPatternMessage.fullMessageCheck(v_220975, v_220975.check()));
+       /* System.out.println(STIGPatternMessage.fullMessageEnforce(v_220968, v_220968.enforce()));
+        System.out.println(STIGPatternMessage.fullMessageCheck(v_220968, v_220968.check()));
+        System.out.println(STIGPatternMessage.fullMessageEnforce(v_220870, v_220870.enforce()));
+        System.out.println(STIGPatternMessage.fullMessageCheck(v_220870, v_220870.check()));
+        System.out.println(STIGPatternMessage.fullMessageEnforce(v_220871, v_220871.enforce()));
+        System.out.println(STIGPatternMessage.fullMessageCheck(v_220871, v_220871.check()));
+        */
+
+
+    }
+}
