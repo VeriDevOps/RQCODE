e94a2d59e87b4dccc6acd00b1682a903b99e8f1b
Andrey Sadovykh <sadovykh@gmail.com>
2024-01-09 11:06:04 +0100

    refactoring

--- src/main/java/rqcode/stigs/win10_v3/Windows10SecurityTechnicalImplementationGuide.java
+++ src/main/java/rqcode/stigs/win10_v3/Windows10SecurityTechnicalImplementationGuide.java
@@ -1,7 +1,6 @@
 package rqcode.stigs.win10_v3;
 
 import rqcode.stigs.STIG;
-import rqcode.stigs.win10_new.patterns.STIGPatternMessage;
 import rqcode.stigs.win10_v3.UserRights.V_220969;
 import rqcode.stigs.win10_v3.UserRights.V_220974;
 import rqcode.stigs.win10_v3.UserRights.V_220975;
@@ -21,7 +20,7 @@ public class Windows10SecurityTechnicalImplementationGuide {
     public static void main(String[] args) {
         System.out.println(v_220982.enforce());
         System.out.println(v_220982.check());
-        System.out.println(v_220982.toString());
+        System.out.println(v_220982);
         //System.out.println(v_220969.check());
         //System.out.println(STIGPatternMessage.fullMessageCheck(v_220972, v_220972.check()));
        // System.out.println(STIGPatternMessage.fullMessageEnforce(v_220975, v_220975.enforce()));
