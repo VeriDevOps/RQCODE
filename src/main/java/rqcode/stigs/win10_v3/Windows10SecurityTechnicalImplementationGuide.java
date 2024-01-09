ea61b0c6dc3b7f4d0898e851ae9beb5be61ddebf
ildar1 <ildarnn22@gmail.com>
2024-01-09 17:54:32 +0300

    Fixed Red Edit STIGs

--- src/main/java/rqcode/stigs/win10_v3/Windows10SecurityTechnicalImplementationGuide.java
+++ src/main/java/rqcode/stigs/win10_v3/Windows10SecurityTechnicalImplementationGuide.java
@@ -8,6 +8,9 @@ import rqcode.stigs.win10_v3.UserRights.V_220975;
 import rqcode.stigs.win10_v3.UserRights.V_220972;
 import rqcode.stigs.win10_v3.UserRights.V_220957;
 import rqcode.stigs.win10_v3.UserRights.V_220982;
+import rqcode.stigs.win10_v3.AuditPolicy.V_220748;
+
+
 
 
 public class Windows10SecurityTechnicalImplementationGuide {
@@ -17,11 +20,12 @@ public class Windows10SecurityTechnicalImplementationGuide {
     public static final STIG v_220972 = new V_220972();
     public static final STIG v_220957 = new V_220957();
     public static final STIG v_220982 = new V_220982();
+    public static final STIG v_220748 = new V_220748();
     
     public static void main(String[] args) {
-        System.out.println(v_220982.enforce());
-        System.out.println(v_220982.check());
-        System.out.println(v_220982.toString());
+        System.out.println(v_220748.enforce());
+        System.out.println(v_220748.check());
+        System.out.println(v_220748.toString());
         //System.out.println(v_220969.check());
         //System.out.println(STIGPatternMessage.fullMessageCheck(v_220972, v_220972.check()));
        // System.out.println(STIGPatternMessage.fullMessageEnforce(v_220975, v_220975.enforce()));
